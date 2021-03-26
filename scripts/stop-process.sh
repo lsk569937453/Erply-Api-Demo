3d39a34abd6b9bdb36aa0e778f70ec13d17c7038
shikai.liu <lsk569937453@gmail.com>
2021-03-26 09:22:46 +0800

    modify the stop scripts

--- scripts/stop-process.sh
+++ scripts/stop-process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ps -ef | grep erply-api | grep -v grep | awk '{print $2}' | xargs kill
-sudo rm -rf /tmp/erply-api/*
+sudo rm -rf /tmp/erply_api/*
 sudo rm -rf /opt/codedeploy-agent/deployment-root/*
